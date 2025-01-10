75a85d2548aeac9ba7eb035eb4b52f64ac15e3cc
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-01-09 21:19:11 -0500

    Add testing commands

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from discord.ext import commands, tasks
 
 # Token and prefix in a separate config.py file, untracked for token purposes
 from config import PREFIX, BOT_TOKEN
+from assets import IMAGES
 from random import choice
 import asyncio
-import datetime
+import aiohttp
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
@@ -43,15 +44,36 @@ async def on_ready():
     change_bot_status.start()
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@bot.command()
+async def test(ctx):
+    await ctx.send(f"Hey {ctx.author.mention}, your test worked!")
 
-    content = message.content
-    tokens = content.split(" ")
-    if tokens[0] == PREFIX + "test":
-        await message.channel.send("Test complete.")
+
+@bot.command(name="embed")
+async def test_embed(
+    ctx, title: str = "Title of embed", desc: str = "Description of embed"
+):
+    embed_msg = discord.Embed(
+        title=title, description=desc, color=discord.Colour(0xFFD1DC)
+    )
+    embed_msg.set_thumbnail(url=IMAGES["LOTUS"])
+    embed_msg.add_field(name="Test field name", value="Test field value", inline=False)
+    embed_msg.add_field(name="Server picture", value="val", inline=False)
+    embed_msg.set_image(url=ctx.guild.icon)
+    embed_msg.set_footer(
+        text="Finally, a footer. Here's your pfp :3", icon_url=ctx.author.avatar
+    )
+    await ctx.send(embed=embed_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    ping_embed = discord.Embed(title="Ping", description="Latency in ms")
+    ping_embed.add_field(
+        name=f"{bot.user.name}'s latency", value=f"{round(bot.latency * 1000)}ms"
+    )
+    ping_embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
+    await ctx.send(embed=ping_embed)
 
 
 async def main():
