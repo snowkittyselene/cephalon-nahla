06582058e39ec42cd63872923c70c07d7084a3de
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-02-19 00:37:29 -0500

    Remove file with unused images

--- assets.py
+++
@@ -1,3 +0,0 @@
-IMAGES = {
-    "LOTUS": "https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2097-CUSA00080_00-AV00000000000006/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=293&h=293",
-}
