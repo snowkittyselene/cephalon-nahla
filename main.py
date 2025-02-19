19f57d137cad1b7216731a4321e57b5296ad61c5
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-02-18 22:37:43 -0500

    Add guild lock for admin commands

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands, tasks
 
 # Token and prefix in a separate config.py file, untracked for token purposes
-from config import PREFIX, BOT_TOKEN, SHUTDOWN_IDS
+from config import PREFIX, BOT_TOKEN, SHUTDOWN_IDS, DEV_GUILD_ID
 from assets import IMAGES
 from random import choice
 import asyncio
@@ -10,6 +10,7 @@ import os
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+dev_guild = None
 
 possible_statuses = [
     "Stalking the Stalker...",
@@ -40,8 +41,32 @@ async def change_bot_status():
 
 @bot.event
 async def on_ready():
+    dev_guild = discord.utils.get(bot.guilds, id=DEV_GUILD_ID)
+    if dev_guild is None:
+        print("Couldn't find dev guild. Shutting down.")
+        return
     print(f"Logged in as {bot.user}!")
     change_bot_status.start()
+
+
+@bot.tree.command(
+    name="resync_commands",
+    description="Re-sync the commands. Usable only by authorised people.",
+    guild=dev_guild,
+)
+async def resync_commands(ctx: discord.Interaction):
+    if ctx.user.id not in SHUTDOWN_IDS:
+        await ctx.response.send_message(
+            f"Only authorised users can use this command.", ephemeral=True
+        )
+        return
+    try:
+        synced_commands = await bot.tree.sync()
+        await ctx.response.send_message(
+            f"Synced {len(synced_commands)} commands", ephemeral=True
+        )
+    except Exception as e:
+        print(f"Error while syncing commands: {e}")
 
 
 @bot.command(
@@ -97,16 +122,18 @@ async def ping(interaction: discord.Interaction):
     await interaction.response.send_message(embed=ping_embed, ephemeral=True)
 
 
-@bot.command(
-    name="shutdown", description="Shuts down the bot. Usable only by authorised users."
+@bot.tree.command(
+    name="shutdown",
+    description="Shuts down the bot. Usable only by authorised users.",
+    guild=dev_guild,
 )
-async def shutdown(ctx):
-    if ctx.author.id not in SHUTDOWN_IDS:
-        await ctx.send(
+async def shutdown(ctx: discord.Interaction):
+    if ctx.user.id not in SHUTDOWN_IDS:
+        await ctx.response.send_message(
             "Sorry, only authorised users can use this command.", ephemeral=True
         )
     else:
-        await ctx.send("Shutting down...")
+        await ctx.response.send_message("Shutting down...")
         print("Closing!")
         await bot.close()
 
@@ -114,16 +141,15 @@ async def shutdown(ctx):
 @bot.tree.command(
     name="reload_cog",
     description="Reloads a specified cog. Usable only by authorised users",
+    guild=dev_guild,
 )
-async def reload_cog(interaction: discord.Interaction, cog_name: str):
-    if interaction.user.id not in SHUTDOWN_IDS:
-        await interaction.response.send_message(
+async def reload_cog(ctx: discord.Interaction, cog_name: str):
+    if ctx.user.id not in SHUTDOWN_IDS:
+        await ctx.response.send_message(
             "Sorry, only authorised users can use this.", ephemeral=True
         )
     else:
-        await interaction.response.send_message(
-            f"Restarting {cog_name}...", ephemeral=True
-        )
+        await ctx.response.send_message(f"Restarting {cog_name}...", ephemeral=True)
         await bot.reload_extension(f"cogs.{cog_name}")
 
 
