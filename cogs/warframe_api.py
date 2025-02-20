72a2795b1f121d19139d36abfd7dea61c1e840f8
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-02-19 21:08:01 -0500

    Add world state and circuit commands

--- cogs/warframe_api.py
+++ cogs/warframe_api.py
@@ -23,12 +23,12 @@ class WarframeAPI(commands.Cog):
         name="baro",
         description="Shows Baro Ki'Teer's current inventory or when he's next due",
     )
-    async def baro(self, original_interaction: discord.Interaction):
+    async def baro(self, ctx: discord.Interaction):
         response = pull_from_api("voidTrader")
         api_data = response.json()
         start = 0
         if not api_data["active"]:
-            await original_interaction.response.send_message(
+            await ctx.response.send_message(
                 f"I'm so sorry, Tenno, but Baro Ki'Teer is currently ~~pregnant~~ scouring the Void for hidden treasures. He will be due to arrive at {api_data["location"]} in approximately {api_data["startString"]}"
             )
             return
@@ -46,7 +46,79 @@ class WarframeAPI(commands.Cog):
         view = PaginationView(inventory, data, ITEMS_PER_PAGE, 30, generate_base)
         if is_one_page:
             view.clear_items()
-        await original_interaction.response.send_message(embed=embed, view=view)
+        await ctx.response.send_message(embed=embed, view=view)
+
+    @ac.command(
+        name="worldstate",
+        description="Gets the current state of the open world locations",
+    )
+    async def worldstate(self, ctx: discord.Interaction):
+        embed = discord.Embed(
+            title="Current world data", description="Open worlds and Earth plant cycle"
+        )
+        # cetus
+        cetus_response = pull_from_api("cetusCycle")
+        cetus_data = cetus_response.json()
+        embed.add_field(
+            name="Plains of Eidolon (Earth)",
+            value=f"{cetus_data["state"].title()} - Changing in {cetus_data["timeLeft"]}",
+            inline=False,
+        )
+        # earth plants
+        plants_reponse = pull_from_api("earthCycle")
+        plants_data = plants_reponse.json()
+        embed.add_field(
+            name="Earth Day/Night Cycle (Plants)",
+            value=f"{plants_data["state"].title()} - Changing in {plants_data["timeLeft"]}",
+            inline=False,
+        )
+        # fortuna
+        fortuna_response = pull_from_api("vallisCycle")
+        fortuna_data = fortuna_response.json()
+        embed.add_field(
+            name="Orb Vallis (Venus)",
+            value=f"{fortuna_data["state"].title()} - Changing in {fortuna_data["timeLeft"]}",
+            inline=False,
+        )
+        # cambion drift
+        deimos_response = pull_from_api("cambionCycle")
+        deimos_data = deimos_response.json()
+        embed.add_field(
+            name="Cambion Drift (Deimos)",
+            value=f"{deimos_data["state"].title()} - Changing in {deimos_data["timeLeft"]}",
+            inline=False,
+        )
+        # duviri
+        duviri_response = pull_from_api("duviriCycle")
+        duviri_data = duviri_response.json()
+        embed.add_field(
+            name="Duviri",
+            value=f"Current spiral: {duviri_data["state"].title()}",
+            inline=False,
+        )
+
+        await ctx.response.send_message(embed=embed)
+
+    @ac.command(name="circuit", description="Gets the current rewards for the Circuit")
+    async def circuit(self, ctx: discord.Interaction):
+        circuit_response = pull_from_api("duviriCycle")
+        circuit_data = circuit_response.json()
+        regular_rewards = circuit_data["choices"][0]["choices"]
+        steel_path_rewards = circuit_data["choices"][1]["choices"]
+        embed = discord.Embed(
+            title="The Circuit - Rewards",
+            description="Warframes and Incarnon Geneses",
+        )
+        embed.add_field(name="Normal Circuit", value="Warframes", inline=False)
+        for reward in regular_rewards:
+            embed.add_field(name=reward, value="")
+        embed.add_field(
+            name="Steel Path Circuit", value="Incarnon Geneses", inline=False
+        )
+        for reward in steel_path_rewards:
+            embed.add_field(name=reward, value="")
+
+        await ctx.response.send_message(embed=embed)
 
 
 async def generate_base(start, inventory, info):
